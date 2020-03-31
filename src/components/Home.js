37ac5232dc84e7f5d8e918662120d7055ea36a3a
Zara <zara@okaayfinee.home>
2020-03-31 21:51:05 +1100

    Starting adding fetchFlights - Zara

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import AllFlights from './AllFlights'
 
 class Home extends Component {
+
+  fetchFlights(q) {
+    const URL = "http://f60893a7.ngrok.io/flights.json";
+    axios.get(URL).then((results) => {
+      this.setState({flights: results.data});
+    });
+  }
+
   render(){
     return(
       <div>
