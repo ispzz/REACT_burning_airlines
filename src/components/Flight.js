fd00bb72d711f3aa43eed3e452b56c31d7e6f6f2
Zara <zara@okaayfinee.home>
2020-04-01 11:05:28 +1100

    Added more flight details to flight link - Zara

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 function Flight(props) {
   return (
     <li>
-      Flight: <a href={props.flight_no}>{props.flight_no}</a>
+      Flight: <a href={props.flight_no}>{props.date} {props.flight_no} {props.source} {props.destination}</a>
     </li>
   );
 }
