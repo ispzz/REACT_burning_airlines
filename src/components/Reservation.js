cabfb7221c300be7f54f7fbd584685660f86bdea
Carmina <cvdsoto@gmail.com>
2020-04-02 11:02:55 +1100

    disabled buttons for reserved seats- mina

--- src/components/Reservation.js
+++ src/components/Reservation.js
@@ -10,8 +10,12 @@ class Reservation extends Component {
   createButtons = s => {
     const seats = [];
     const totalSeats = s;
+    // let buttonStyle;
+    // let disabled;
     for (let i = 1; i <= totalSeats; i++) {
+      //row determines the number. divide first number by 4 (4 seats/row)
       const row = Math.ceil(i / 4);
+      //column determines the letter. get the remainder to determine letter
       const column = i % 4;
       let seatNumber;
       if (column === 1) {
@@ -24,12 +28,22 @@ class Reservation extends Component {
         seatNumber = "D";
       }
       seatNumber = row.toString() + seatNumber;
+      // disabled = '';
+      // buttonStyle = '';
+      // if (this.props.seat.includes(seatNumber)){
+      //   disabled = true;
+      //   buttonStyle = this.props.seatStyling.reserved;
+      // } else {
+      //   buttonStyle = this.props.seatStyling.active;
+      // }
       seats.push(
         <button
-          className={this.props.clicked? this.props.seatStyling.selected : this.props.seatStyling.active} // style will be .active or .reserved
+          className={this.props.clicked ? this.props.seatStyling.selected : this.props.seatStyling.active}
+          // style will be .active or .reserved
           type="button"
           onClick={this.props.clickSeatSelection}
           id={seatNumber}
+          // disabled={disabled}
         >
           {seatNumber}
         </button>
